e23adc132dfdc1b5c37b41ee605bce225f3e5685
Juraj Michálek <juraj.michalek@espressif.com>
2022-05-05 14:23:55 +0200

    add gitpod support

--- server.py
+++ server.py
@@ -5,9 +5,10 @@ import base64
 import json
 import sys
 import os
+import subprocess
 import websockets
 import webbrowser
-
+import time
 
 PORT = 9012
 
@@ -26,8 +27,8 @@ async def hello(websocket, path):
         "type": "start",
         "elf": base64_file('{}/wokwi/dummy.elf'.format(os.getcwd())),
         "espBin": [
-            [0x0000, base64_file('{}/config-files/{}_bootloader.bin'.format(os.getcwd(), os.getenv('ESP_BOARD')))],
-            [0x8000, base64_file('{}/config-files/{}_partition-table.bin'.format(os.getcwd(), os.getenv('ESP_BOARD')))],
+            [0x0000, base64_file('{}/wokwi/esp32c3_bootloader.bin'.format(os.getcwd()))],
+            [0x8000, base64_file('{}/wokwi/esp32c3_partition-table.bin'.format(os.getcwd()))],
             [0x10000, base64_file('{}/app.bin'.format(os.getenv('CURRENT_PROJECT')))],
         ]
     }))
@@ -43,20 +44,26 @@ async def hello(websocket, path):
 
 start_server = websockets.serve(hello, "127.0.0.1", PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
-board = 325149339656651346
-if os.getenv('ESP_BOARD') == "esp32c3":
-    board = 325149339656651346
-elif os.getenv('ESP_BOARD') == "esp32c3-rust":
-    board = 328638850887844436
-elif os.getenv('ESP_BOARD') == "esp32":
-    board = 329992166987268691
-# else :
-#     return 1
 
-url = "https://wokwi.com/_alpha/wembed/{}?partner=espressif&port={}&data=demo".format(board,PORT)
-print("Web socket listening on port {}".format(PORT))
-print("")
+# ESP32-C3-DevKitC-02
+# board = 325149339656651346
+# ESP32C3 Rust Board
+board = 328638850887844436
+if "intro/hardware-check" in os.getenv('CURRENT_PROJECT') or "intro/mqtt" in os.getenv('CURRENT_PROJECT') or "advanced/button-interrupt" in os.getenv('CURRENT_PROJECT'):
+    # ESP32C3 Rust Board with Neopixel
+    board = 328904135759888980
+elif "advanced/i2c-driver" in os.getenv('CURRENT_PROJECT'):
+    board = 329811477748777556
+if(os.getenv('USER') == "gitpod"):
+    gp_url = subprocess.getoutput("gp url {}".format(PORT))
+    gp_url = gp_url[8:]
+    url = "https://wokwi.com/_alpha/wembed/{}?partner=espressif&port={}&data=demo&_host={}".format(board,PORT,gp_url)
+else:
+    url = "https://wokwi.com/_alpha/wembed/{}?partner=espressif&port={}&data=demo".format(board,PORT)
 print("Please, open the following URL: {}".format(url))
-webbrowser.open(url)
+if(os.getenv('USER') == "gitpod"):
+    time.sleep(2)
+    open_preview = subprocess.getoutput("gp preview \"{}\"".format(url))
+else:
+    webbrowser.open(url)
 asyncio.get_event_loop().run_forever()
-
